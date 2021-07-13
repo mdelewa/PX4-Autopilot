b391b1c0822c1c0eef89b80255a03d8309fe8564
gemie <Mahmoud.Elewa98@eng-st.cu.edu.eg>
2021-07-13 09:32:46 +0200

    longitudinal implementation ready

---
+++ src/modules/fw_att_control/lqr_longitudinal_controller.cpp
@@ -0,0 +1,134 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2021 HAPS Control Graduation Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name HAPS Control GP nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file lqr_longitudinal_controller.cpp
+ * Implementation of a longitudinal pitch LQR controller.
+ *
+ * Authors and acknowledgements in header.
+ */
+
+#include "lqr_longitudinal_controller.h"
+#include <float.h>
+#include <lib/ecl/geo/geo.h>
+#include <mathlib/mathlib.h>
+
+LQR_LONGITUDINAL_CONTROLLER::LQR_LONGITUDINAL_CONTROLLER() :
+	_u0(0.0f),
+	_w0(0.0f),
+	_q0(0.0f),
+	_th0(0.0f),
+	_k_ele_u(0.0f),
+	_k_ele_w(0.0f),
+	_k_ele_q(0.0f),
+	_k_ele_th(0.0f),
+	_k_ele_intg_th(0.0f),
+	_integrator_max(0.0f),
+	_last_elevator_output(0.0f),
+	_pitch_error_integrator(0.0f),
+	_pitch_error(0.0f),
+{
+}
+
+void LQR_LONGITUDINAL_CONTROLLER::reset_integrator()
+{
+	_pitch_error_integrator = 0.0f;
+}
+
+float LQR_LONGITUDINAL_CONTROLLER::constrain_airspeed(float airspeed, float minspeed, float maxspeed)
+{
+	float airspeed_result = airspeed;
+
+	if (!PX4_ISFINITE(airspeed)) {
+		/* airspeed is NaN, +- INF or not available, pick center of band */
+		airspeed_result = 0.5f * (minspeed + maxspeed);
+
+	} else if (airspeed < minspeed) {
+		airspeed_result = minspeed;
+	}
+
+	return airspeed_result;
+}
+
+float LQR_LONGITUDINAL_CONTROLLER::control_attitude_elevator_LQR(const float dt, const LQR_ControlData &ctl_data)
+{
+	/* Do not calculate control signal with bad inputs */
+	if (!(PX4_ISFINITE(ctl_data.u) &&
+	      PX4_ISFINITE(ctl_data.w) &&
+	      PX4_ISFINITE(ctl_data.pitch) &&
+	      PX4_ISFINITE(ctl_data.body_y_rate) &&
+	      PX4_ISFINITE(ctl_data.pitch_setpoint) &&
+	      PX4_ISFINITE(ctl_data.airspeed_min) &&
+	      PX4_ISFINITE(ctl_data.airspeed_max) &&
+	      PX4_ISFINITE(ctl_data.scaler))) {
+
+		return math::constrain(_last_elevator_output, -1.0f, 1.0f);
+	}
+
+	/* Calculate pitch error */
+	_pitch_error = ctl_data.pitch_setpoint - ctl_data.pitch;
+
+	/* Calculate delta_states */
+	float delta_u  = ctl_data.u-_u0;
+	float delta_w  = ctl_data.w-_w0;
+	float delta_q  = ctl_data.body_y_rate-_q0;
+	float delta_th = ctl_data.pitch-_th0;
+
+	if (!ctl_data.lock_integrator) {
+
+		/* Integral term */
+		float id = _pitch_error * dt;
+
+		/*
+		 * anti-windup: do not allow integrator to increase if actuator is at limit
+		 */
+		if (_last_elevator_output < -1.0f) {
+			/* only allow motion to center: increase value */
+			id = math::max(id, 0.0f);
+
+		} else if (_last_elevator_output > 1.0f) {
+			/* only allow motion to center: decrease value */
+			id = math::min(id, 0.0f);
+		}
+
+		/* add and constrain */
+		_pitch_error_integrator = math::constrain(_pitch_error_integrator + id , -_integrator_max, _integrator_max);
+	}
+
+	/* Apply LQR controller and store non-limited output */
+	_last_elevator_output = _k_ele_u * delta_u + _k_ele_w * delta_w + _k_ele_q * delta_q + _k_ele_th * delta_th + _k_ele_intg_th * _pitch_error_integrator;
+	/* u = kx not u ~= -kx due to different sign convention for servo */
+	_last_elevator_output = _last_elevator_output*1; // u ~= -kx not
+	_last_elevator_output = _last_elevator_output*2;
+	return math::constrain(_last_elevator_output, -1.0f, 1.0f);
+}
---
+++ src/modules/fw_att_control/lqr_longitudinal_controller.hpp
@@ -0,0 +1,134 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2021 HAPS Control Graduation Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name HAPS Control GP nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file lqr_longitudinal_controller.h
+ * Definition of class for longitudinal controller
+ *
+ * @author Mahmoud Elewa <mhdgamal98@gmail.com>
+ *
+ * Acknowledgements:
+ *
+ *   The control design is based on a design
+ *   by Youssef Madany and Mahmoud ElQoulaly, 2021,
+ *   which can be found as a part of HAPS control
+ *   graduation project 2021
+ *   Faculty of Engineering, Cairo University.
+ */
+
+#ifndef LQR_LONGITUDINAL_CONTROLLER_H
+#define LQR_LONGITUDINAL_CONTROLLER_H
+
+#include <mathlib/mathlib.h>
+#include <px4_log.h>
+
+struct LQR_ControlData {
+	float u;
+	float v;
+	float w;
+	float roll;
+	float pitch;
+	float yaw;
+	float body_x_rate;
+	float body_y_rate;
+	float body_z_rate;
+	float roll_setpoint;
+	float pitch_setpoint;
+	float yaw_setpoint;
+	float roll_rate_setpoint;
+	float pitch_rate_setpoint;
+	float yaw_rate_setpoint;
+	float airspeed_min;
+	float airspeed_max;
+	float airspeed;
+	float scaler;
+	float groundspeed;
+	float groundspeed_scaler;
+	bool lock_integrator;
+};
+
+class LQR_LONGITUDINAL_CONTROLLER
+{
+public:
+	LQR_LONGITUDINAL_CONTROLLER();
+	~LQR_LONGITUDINAL_CONTROLLER() = default;
+
+	float control_attitude_elevator_LQR(const float dt, const LQR_ControlData &ctl_data);
+
+	/* Setters */
+	void set_states_0(float u_0, float w_0, float q_0, float th_0)
+	{
+		_u0  = u_0;
+		_w0  = w_0;
+		_q0  = q_0;
+		_th0 = th_0;
+	}
+
+	void set_lqr_gains(float gain_u, float gain_w, float gain_q, float gain_th,  float gain_intg_th)
+	{
+		_k_ele_u       = gain_u;
+		_k_ele_w       = gain_w;
+		_k_ele_q       = gain_q;
+		_k_ele_th      = gain_th;
+		_k_ele_intg_th = gain_intg_th;
+	}
+
+	void set_integrator_max(float max){ _integrator_max = max; }
+
+	/* Getters */
+	float get_integrator(){	return _pitch_error_integrator;	}
+
+	void reset_integrator(){ _pitch_error_integrator = 0.0f; }
+
+
+private:
+	float _u0;
+	float _w0;
+	float _q0;
+	float _th0;
+
+	float _k_ele_u;
+	float _k_ele_w;
+	float _k_ele_q;
+	float _k_ele_th;
+	float _k_ele_intg_th;
+
+	float _pitch_error;
+
+	float _integrator_max;
+	float _last_elevator_output;
+	float _pitch_error_integrator;
+	float constrain_airspeed(float airspeed, float minspeed, float maxspeed);
+};
+
+#endif // ECL_PITCH_CONTROLLER_H
